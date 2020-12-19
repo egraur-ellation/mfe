448d91cfb15e4dcaf3f91b2911e12190c7d241f5
egraur <efim.graur@ellation.com>
2020-12-19 07:02:21 +0200

    Created container deploy script

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,30 @@
+//need this to merge common and prod
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+//webpack to take care of shared modules for us
+const packageJson = require('../package.json');
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    //when we build some files for production all the files built will use
+    //this template in naming them
+    //done for caching issues
+    filename: '[name].[contenthash].js',
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      //where we should go to get some source code
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`
+      },
+      shared: packageJson.dependencies
+    })
+  ]
+};
+
+module.exports = merge(commonConfig,prodConfig);
